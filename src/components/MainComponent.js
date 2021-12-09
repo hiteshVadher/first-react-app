de23ecbf3af5266649ea205c3770601839bef076
Hitesh Vadher <hiteshvadher099@gmail.com>
2021-12-09 18:13:00 +0530

    single page application part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import Menu from './MenuComponents';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Router, Route, Routes, Navigate } from 'react-router-dom';
 
 class Main extends Component {
@@ -14,11 +18,10 @@ class Main extends Component {
 
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
     }
 
     render() {
@@ -26,10 +29,16 @@ class Main extends Component {
             <div>
                 <Header />
                 <Routes>
-                    <Route path="/home" element={<Home/>} />
+                    <Route path="/home" element={<Home 
+                        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                        leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+                    } />
                     <Route exact path="/menu" element={<Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" element={<Contact/>} />
                     <Route path="*" element={<Navigate to = "/home" />} />
                 </Routes>
+                <Footer />
             </div>
         );
     }
